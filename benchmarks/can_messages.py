66ec12ec4be6a89792c531e3b970b60526b20e15
Jev <jev.kuznetsov@gmail.com>
2024-07-28 12:22:42 +0000

    add proto

--- benchmarks/can_messages.py
+++ benchmarks/can_messages.py
@@ -10,7 +10,8 @@ parse: 0.174 ms
 
 import time
 import gc
-
+from collections import namedtuple
+import struct
 import can_protocol as protocol
 
 node_id = 10
@@ -51,3 +52,22 @@ t_end = time.monotonic_ns()
 t_elapsed = (t_end - t_start) / 1e6
 t_parse = t_elapsed / iterations
 print(f"parse: {t_parse:.3f} ms")
+
+
+# -------- proto simple protocol --------
+
+# parsing without creating separate message classes
+
+Heartbeat = namedtuple("Heartbeat", "error_code error_count uptime version")
+byte_def = "<BHIB"
+opcode = 1
+msg_id = protocol.generate_message_id(msg.opcode, node_id)
+
+t_start = time.monotonic_ns()
+for _ in range(iterations):
+    opcode, node_id = protocol.split_message_id(msg_id)
+    msg = struct.unpack(byte_def, b"\x01\x02\x00\x03\x00\x00\x00\x07")
+t_end = time.monotonic_ns()
+t_elapsed = (t_end - t_start) / 1e6
+t_proto = t_elapsed / iterations
+print(f"proto: {t_proto:.3f} ms")
