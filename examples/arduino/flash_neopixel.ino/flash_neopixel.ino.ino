a0146b373565e943dae5dc67041a648214c4b1c4
Jev <jev.kuznetsov@gmail.com>
2024-09-04 13:16:45 +0200

    add neopixel demo

---
+++ examples/arduino/flash_neopixel.ino/flash_neopixel.ino.ino
@@ -0,0 +1,30 @@
+#include <Adafruit_NeoPixel.h>
+
+Adafruit_NeoPixel strip(1, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+
+// Define colors: Green, Blue, Red
+const uint32_t colors[] = {0x00FF00, 0x0000FF, 0xFF0000};
+const int numColors = sizeof(colors) / sizeof(colors[0]);
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) delay(10);  // Wait for Serial Monitor to open
+
+  Serial.print("Running neopixel demo ");
+
+  strip.begin();
+  strip.setBrightness(50);
+  strip.show();  // Initialize all pixels to 'off'
+}
+
+void setColor(uint32_t color) {
+  strip.setPixelColor(0, color);
+  strip.show();
+  delay(200);
+}
+
+void loop() {
+  for (int i = 0; i < numColors; i++) {
+    setColor(colors[i]);
+  }
+}
