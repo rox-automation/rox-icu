11c8d10a7a13097e0ff146218607ba8f819527b0
Jev <jev@roxautomation.com>
2024-11-10 17:24:16 +0000

    option to set output

--- src/rox_icu/cli.py
+++ src/rox_icu/cli.py
@@ -3,9 +3,14 @@
 rox_icu CLI
 """
 
+import can
 import click
 from rox_icu import __version__
-from .utils import run_main
+from rox_icu.utils import run_main
+import rox_icu.can_protocol as canp
+
+
+DEFAULT_INTERFACE = "slcan0"
 
 
 @click.group()
@@ -22,7 +27,7 @@ def info() -> None:
 
 @cli.command()
 @click.option("--node-id", default=1, help="device ID")
-@click.option("--interface", default="vcan0", help="CAN interface")
+@click.option("--interface", default=DEFAULT_INTERFACE, help="CAN interface")
 def mock(node_id, interface):
     """Mock ICU device on CAN bus"""
     from .mock import main
@@ -31,7 +36,7 @@ def mock(node_id, interface):
 
 
 @cli.command()
-@click.option("--interface", default="vcan0", help="CAN interface")
+@click.option("--interface", default=DEFAULT_INTERFACE, help="CAN interface")
 def monitor(interface):
     """Monitor ICU devices on CAN bus"""
     import curses
@@ -40,6 +45,29 @@ def monitor(interface):
     monitor.INTERFACE = interface
 
     curses.wrapper(monitor.main)
+
+
+@cli.command()
+@click.argument("hex_input")
+@click.option("--node-id", default=1, help="device ID")
+@click.option("--interface", default=DEFAULT_INTERFACE, help="CAN interface")
+def output(hex_input: str, node_id: int, interface: str):
+    """Set output state, provide hex value"""
+    try:
+        state = int(hex_input, 16)
+
+    except ValueError:
+        print("Invalid hex input. Please provide a valid hexadecimal value.")
+        return
+
+    # for now, simply put packet on the bus, without checking if device is alive
+
+    print(f"Setting ICU_{node_id} on {interface} output state to {state:08b}")
+
+    with can.interface.Bus(channel=interface, bustype="socketcan") as bus:
+        arb_id, data = canp.encode_message(canp.IOStateMessage(state), node_id)
+        msg = can.Message(arbitration_id=arb_id, data=data)
+        bus.send(msg)
 
 
 if __name__ == "__main__":
