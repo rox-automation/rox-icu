7432dc1c1727cd5fadd12410cc5a9d9a76229ef2
Jev <jev.kuznetsov@gmail.com>
2025-02-23 21:45:44 +0000

    update message encoding to use SetIoStateMessage in ICU class

--- src/rox_icu/core.py
+++ src/rox_icu/core.py
@@ -152,7 +152,7 @@ class ICU:
         self.check_alive()
 
         arb_id, msg_data = canp.encode_message(
-            canp.IoStateMessage(state), node_id=self._node_id
+            canp.SetIoStateMessage(state), node_id=self._node_id
         )
 
         self._bus.send(
